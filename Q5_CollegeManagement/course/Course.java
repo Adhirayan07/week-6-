487116e3cafea02e79c27be9590dc7b0c5af096d
Adhirayan07 <adhirayan.baskar@gmail.com>
2026-09-17 13:45:31 +0530

    Add Q5 Course package

---
+++ Q5_CollegeManagement/course/Course.java
@@ -0,0 +1,16 @@
+package Q5_CollegeManagement.course;
+
+public class Course {
+    private String courseCode;
+    private String courseName;
+
+    public Course(String courseCode, String courseName) {
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+    }
+
+    public void display() {
+        System.out.println("Course Code: " + courseCode);
+        System.out.println("Course Name: " + courseName);
+    }
+}
