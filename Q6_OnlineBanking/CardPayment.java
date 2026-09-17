ff17d3edef4bbbec75d7aea0c868bcc03d9c2e05
Adhirayan07 <adhirayan.baskar@gmail.com>
2026-09-17 13:46:14 +0530

    Add Q6 card payment

---
+++ Q6_OnlineBanking/CardPayment.java
@@ -0,0 +1,7 @@
+package Q6_OnlineBanking;
+
+public class CardPayment implements Payment, OnlineTransaction {
+    public void pay(double amount) {
+        System.out.println("Paid ₹" + amount + " using Card");
+    }
+}
